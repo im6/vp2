447826c884596d5994032e3b84186456273e4b6b
jinouwuque <ee2win@gmail.com>
2017-12-10 07:02:21 -0500

    add sql connection and admin access

--- colorpk/settings.py
+++ colorpk/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'colorpk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('SQL_DATABASE'),
+        'USER': os.environ.get('SQL_USERNAME'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD'),
+        'HOST': os.environ.get('SQL_HOST'),
+        'PORT': os.environ.get('SQL_PORT'),
     }
 }
 
