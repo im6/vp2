6b0ead69db38dd31694a3c95ebc2556525f797c3
jinouwuque <ee2win@gmail.com>
2017-12-24 23:51:21 -0500

    setup the initial js

---
+++ client/index.js
@@ -0,0 +1,4 @@
+const c = [1,2,3,4,5];
+c.forEach(v => {
+  alert(v);
+});
---
+++ client/webpack/dev.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  watch: true,
+  entry: './client/index.js',
+  module:{
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            "presets": ['env']
+          }
+        }]
+      },
+    ],
+  },
+  output: {
+    publicPath: '/static',
+    path: path.join(__dirname, '../../static'),
+    filename: 'bundle.js'
+  },
+};
