6c7f252dacb044aef29fd2faecda4ec6591814d1
jinouwuque <ee2win@gmail.com>
2017-12-25 21:25:26 -0500

    add hoverover effect

--- client/colors/index.js
+++ client/colors/index.js
@@ -2,11 +2,12 @@ import './style.scss';
 
 const boxes = document.getElementsByClassName('box');
 for(let i = 0; i < boxes.length; i ++){
-  let button = boxes[i];
-  let id = button.attributes.getNamedItem('data-k').value;
-  button.getElementsByTagName('button')[0].onclick = (v) => {
-    let imgElem = button.getElementsByTagName('img')[0];
-    let txtElem = button.getElementsByTagName('span')[0];
+  let thisBox = boxes[i];
+  let id = thisBox.attributes.getNamedItem('data-k').value;
+  thisBox.getElementsByTagName('button')[0].onclick = (v) => {
+    let imgElem = thisBox.getElementsByTagName('img')[0];
+    let txtElem = thisBox.getElementsByTagName('span')[4];
+
     let likeNum = parseInt(txtElem.innerText);
     if(imgElem.src.indexOf('hrt.svg') > -1){
       imgElem.src = imgElem.src.replace('hrt.svg', 'hrtr.svg');
