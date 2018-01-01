2893e6416fa56e399eea7272443d9e399b9d3657
jinouwuque <ee2win@gmail.com>
2017-12-31 23:56:37 -0500

    add reset button handler

--- client/newcolor/index.js
+++ client/newcolor/index.js
@@ -1,6 +1,23 @@
 import './style.scss';
 import './jscolor.min';
 
+const state = [null,null,null,null];
+const bars = document.getElementsByClassName('jscolor');
+
+const resetColors = () => {
+  for(let i = 0; i < 4; i ++){
+    bars[i].jscolor.fromString('ffffff');
+  }
+};
+
 window.setTextColor = (picker, id) => {
-  console.log(id);
+  state[id] = picker.toString();
+};
+
+document.getElementById('createBtn').onclick = () => {
+  debugger;
+};
+
+document.getElementById('resetBtn').onclick = () => {
+  resetColors();
 };
