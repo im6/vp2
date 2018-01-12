983ae8a0abcecdd251daf2271cfa73145bb94d09
jinouwuque <ee2win@gmail.com>
2018-01-11 23:35:11 -0500

    add like cache function

--- colorpk/cache_storage.py
+++ colorpk/cache_storage.py
@@ -3,12 +3,23 @@ from colorpk.models.db import Color
 
 init_colors = list(map(lambda x : x.to_dict(), Color.objects.all()))
 cache.set('global_colors', init_colors)
+cache.set('global_like', {})
 
 def getColors():
-    return cache.get('global_colors')
+    rawValue = cache.get('global_colors')
+    like_obj = cache.get('global_like')
+    for one in rawValue:
+        if one.get('id') in like_obj and like_obj.get(one.get('id')) > 0:
+            one['like'] += like_obj.get(one.get('id'))
+    return rawValue
 
 def getColor(id):
     cached_data = cache.get('global_colors')
     filter0 = list(filter(lambda v : v.get('id') == id, cached_data))[0]
     return filter0
 
+def like(id):
+    like_obj = cache.get('global_like')
+    current_like = like_obj.get(id, 0)
+    like_obj[id] = current_like + 1
+    cache.set('global_like', like_obj)
--- colorpk/controller.py
+++ colorpk/controller.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse,JsonResponse
 import json
+import colorpk.cache_storage as cache
 
 def toggleLike(request, id):
     body_unicode = request.body.decode('utf-8')
     body = json.loads(body_unicode)
+    cache.like(id)
     return JsonResponse({
         "color": id
     })
