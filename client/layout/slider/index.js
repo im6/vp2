1c951151dca9d4b2cec2a7f0a12c370a2931e15d
jinouwuque <ee2win@gmail.com>
2018-01-13 14:53:49 -0500

    fix the nav bar

--- client/layout/slider/index.js
+++ client/layout/slider/index.js
@@ -5,7 +5,7 @@ import Slideout from 'slideout';
 var slideout = new Slideout({
   'panel': document.getElementsByTagName('main')[0],
   'menu': document.getElementsByTagName('nav')[0],
-  'padding': 256,
+  'padding': 210,
   'tolerance': 70
 });
 
