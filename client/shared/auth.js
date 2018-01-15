dee069b470722c97db6c6511f2112e41b66fb8dd
jinouwuque <ee2win@gmail.com>
2018-01-14 20:59:26 -0500

    dynamic render the auth status

--- client/shared/auth.js
+++ client/shared/auth.js
@@ -1,13 +1,21 @@
 import { ajax } from './util';
 
-const toggleNav = (user) => {
-  const navElem = document.querySelector('nav > ul');
-  const signInElem = document.querySelector('.nav-signin');
-  const userElem = document.createElement('li');
-  userElem.className = 'nav-user';
-  userElem.innerHTML = `<a href="/profile"><img src="${user.img}" alt="user icon" title="Go to ${user.name}'s Profile"></a>`;
-  navElem.removeChild(signInElem);
-  navElem.prepend(userElem);
+
+const navElem = document.querySelector('nav');
+
+const renderUser = (user) => {
+  const elem = document.createElement('div');
+  elem.className = 'nav-user';
+  elem.innerHTML = `<img src="${user.img}" alt="icon"><div><a href="/profile">${user.name}</a><a href="/signout">Sign out</a></div>`;
+  navElem.appendChild(elem);
+};
+
+const renderAuth = () => {
+  const elem = document.createElement('a');
+  a.className = 'nav-auth';
+  a.href = '/signin';
+  a.innerText = 'Sign In';
+  navElem.appendChild(a);
 };
 
 ajax({
@@ -15,7 +23,9 @@ ajax({
   url: '/userDetail',
   success: ({user}) => {
     if(user){
-      toggleNav(user)
+      renderUser(user)
+    } else {
+      renderAuth();
     }
   },
   fail: () => {
