8a29f154914082c290cf7f6b63646ac67826a6b3
im6 <ee2win@gmail.com>
2018-02-04 12:36:22 -0500

    update allow host

--- vp2/settings/prod.py
+++ vp2/settings/prod.py
@@ -7,5 +7,5 @@ INSTALLED_APPS = [
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
-ALLOWED_HOSTS = ['.colorpk.com']
+ALLOWED_HOSTS = ['localhost']
 STATIC_URL = '//dkny.oss-cn-hangzhou.aliyuncs.com/4/'
