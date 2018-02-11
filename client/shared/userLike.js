d7e50a14a2333e9ffb1cd594db24169a6662dd79
im6 <ee2win@gmail.com>
2018-02-11 17:36:15 -0500

    fix for the private mode

--- client/shared/userLike.js
+++ client/shared/userLike.js
@@ -8,37 +8,54 @@ const clearLocalStorage = () => {
   }
 };
 
-clearLocalStorage();
-
-if('likes' in window._colorpk && window._colorpk.likes.length > 0){
-  window.localStorage.setItem(LSLIKEKEY, JSON.stringify(window._colorpk.likes));
-} else if(window.localStorage.getItem(LSLIKEKEY)){
-  // valid cachestorage
-} else {
-  window.localStorage.setItem(LSLIKEKEY, JSON.stringify([]));
+try {
+  clearLocalStorage();
+  if('likes' in window._colorpk && window._colorpk.likes.length > 0){
+    window.localStorage.setItem(LSLIKEKEY, JSON.stringify(window._colorpk.likes));
+  } else if(window.localStorage.getItem(LSLIKEKEY)){
+    // valid cachestorage
+  } else {
+    window.localStorage.setItem(LSLIKEKEY, JSON.stringify([]));
+  }
+}
+catch (e) {
+  console.warn('localstorage access denied.');
 }
 
 export const getUserLikes = () => {
   let userLike = [];
-  if('likes' in window._colorpk && window._colorpk.likes.length > 0){
-    userLike = window._colorpk.likes;
-    window.localStorage.setItem(LSLIKEKEY, JSON.stringify(userLike));
-  } else if(window.localStorage.getItem(LSLIKEKEY)){
-    userLike = JSON.parse(window.localStorage.getItem(LSLIKEKEY));
+  try {
+    if('likes' in window._colorpk && window._colorpk.likes.length > 0){
+      userLike = window._colorpk.likes;
+      window.localStorage.setItem(LSLIKEKEY, JSON.stringify(userLike));
+    } else if(window.localStorage.getItem(LSLIKEKEY)){
+      userLike = JSON.parse(window.localStorage.getItem(LSLIKEKEY));
+    }
+  } catch(e) {
+    console.warn('localstorage access denied.');
   }
+
   return userLike;
 };
 
 export const addLike = (id) => {
-  let userLike = JSON.parse(window.localStorage.getItem(LSLIKEKEY));
-  userLike.push(id);
-  window.localStorage.setItem(LSLIKEKEY, JSON.stringify(userLike));
+  try {
+    let userLike = JSON.parse(window.localStorage.getItem(LSLIKEKEY));
+    userLike.push(id);
+    window.localStorage.setItem(LSLIKEKEY, JSON.stringify(userLike));
+  } catch(e){
+    console.warn('localstorage access denied.');
+  }
 };
 
 export const removeLike = (id) => {
-  let userLike = JSON.parse(window.localStorage.getItem(LSLIKEKEY));
-  userLike = userLike.filter(v => {
-    return v !== id;
-  });
-  window.localStorage.setItem(LSLIKEKEY, JSON.stringify(userLike));
+  try {
+    let userLike = JSON.parse(window.localStorage.getItem(LSLIKEKEY));
+    userLike = userLike.filter(v => {
+      return v !== id;
+    });
+    window.localStorage.setItem(LSLIKEKEY, JSON.stringify(userLike));
+  } catch(e){
+    console.warn('localstorage access denied.');
+  }
 };
