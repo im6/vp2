100fcd541b61bdc87aa0105eab6b44e72e0e9958
im6 <ee2win@gmail.com>
2018-03-11 11:29:07 -0400

    small border

--- client/colors/layout.js
+++ client/colors/layout.js
@@ -4,21 +4,23 @@ import debounce from 'debounce';
 import { isMobile } from '../shared/util';
 import { checkWelcome, hideWelcome } from '../shared/userPreference';
 
-let BOXWD = null;
+let BOXWD = null, BOXMARGINH = null;
 let wiw = window.innerWidth;
 if(wiw < 321){
-  BOXWD = 145;
+  BOXMARGINH = 4;
+  BOXWD = 125 + (6 + BOXMARGINH) * 2;
 } else if(wiw < 769){
-  BOXWD = 173;
+  BOXMARGINH = 4;
+  BOXWD = 150 + (6 + BOXMARGINH) * 2;
 } else {
-  BOXWD = 260;
+  BOXMARGINH = 10;
+  BOXWD = 220 + (7 + BOXMARGINH) * 2; //wide screen
 }
 
 const mainElem = document.getElementsByClassName('list')[0],
   helpElems = document.getElementsByClassName('help'),
   SPACEPERCENT = isMobile ? 0.99 : 0.9,
   MAXNUM = 6,
-  BOXMARGINH = isMobile ? 8 : 20,
   isWelcomeHidden = checkWelcome();
 
 const helpElem = helpElems.length > 0 ? helpElems[0] : null;
@@ -27,8 +29,8 @@ const adjustLayout = (w) => {
   const wd = Math.floor(w * SPACEPERCENT/BOXWD) * BOXWD;
   mainElem.style.width = wd + 'px';
   mainElem.style.maxWidth = `${BOXWD * MAXNUM}px`;
-  if(helpElem){
-    helpElem.style.width = (wd - BOXMARGINH) + 'px';
+  if(helpElem) {
+    helpElem.style.width = (wd - BOXMARGINH * 2) + 'px';
   }
 };
 
