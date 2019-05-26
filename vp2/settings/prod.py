9d85686c79bcdc6076779198c1533eb4df1cf329
im6 <ee2win@gmail.com>
2019-05-26 14:34:10 -0400

    update

--- vp2/settings/prod.py
+++ vp2/settings/prod.py
@@ -9,3 +9,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['.colorpk.com']
 SECRET_KEY = os.environ.get('SECRET_KEY')
 STATIC_URL = '//dkny.oss-cn-hangzhou.aliyuncs.com/4/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
