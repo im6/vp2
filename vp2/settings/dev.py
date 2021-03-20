d03f1df411b97d6458d9ae705efdf1b626223aa2
im6 <ee2win@gmail.com>
2021-03-20 18:15:19 +0000

    update onecolor name mapping

--- vp2/settings/dev.py
+++ vp2/settings/dev.py
@@ -6,7 +6,10 @@ INSTALLED_APPS = [
 ]
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  'localhost',
+  '192.168.1.249'
+]
 SECRET_KEY = 'helloWorld'
 if os.environ.get('HOT') == 'True':
     STATIC_URL = '/hot/'
